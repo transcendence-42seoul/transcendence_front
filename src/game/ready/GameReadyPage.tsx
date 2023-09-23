ecb9aa84a2a84c7b9bb50a3dbfd3178ab1d6a0e8
seokunee <xocoolree@gmail.com>
2023-09-23 18:27:59 +0900

    [feat] routing 기본 세팅 및 tailwind 기본 세팅

---
+++ src/game/ready/GameReadyPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GameReadyPage() {
+  return (
+    <div>
+      <span className="text-3xl font-bold underline">GameReadyPage</span>
+    </div>
+  );
+}
+
+export default GameReadyPage;
