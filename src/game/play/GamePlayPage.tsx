aec9ed79650ac012a53dc839dec41a93ab8de605
gtd9511 <ydj02109@naver.com>
2023-11-02 17:02:04 +0900

    feat(result-page): making game result page

--- src/game/play/GamePlayPage.tsx
+++ src/game/play/GamePlayPage.tsx
@@ -41,38 +41,75 @@ function GamePlayPage() {
   const [userAScore, setUserAScore] = useState(0);
   const [userBScore, setUserBScore] = useState(0);
 
+  // const gamePlaying = (userASetScore === 2 || userBSetScore === 2);
+
   return (
-    <div className="flex flex-col items-center h-screen max-h-screen w-screen max-w-screen pt-12">
-      <h1 className="text-3xl h-[5%] font-bold mb-10">GamePlayPage</h1>
+    // {(userASetScore === 2 || userBSetScore === 2) ? (
+    //result
+      <div className="flex flex-col items-center h-screen max-h-screen w-screen max-w-screen pt-12">
+      <h1 className="text-3xl h-[5%] font-bold mb-10">Game Result</h1>
       <div className="w-full h-[85%] flex justify-center">
         <div className="w-full lg:w-8/12 h-full mx-5">
-          <div className="flex bg-slate-200 h-[8rem] justify-evenly">
+          <div className="flex bg-red-200 h-full justify-evenly">
             <SmallUserProfile
               mode="Ladder"
               avatarData={userA_avatar}
               recordData={userA_record_}
             />
             <div className="flex w-1/6 items-center">
-              <h1 className="text-4xl">{`${userASetScore}`}</h1>
-              <div className="flex flex-col items-center justify-center">
-                <h1 className="text-2xl">Set</h1>
-                <h3 className="mx-12 whitespace-nowrap text-2xl">{`${userAScore}   :   ${userBScore}`}</h3>
-              </div>
-              <h1 className="text-4xl">{`${userBSetScore}`}</h1>
+            <div>
+              <h1 className="text-3xl h-[5%] font-bold mb-10">You Win</h1>
             </div>
-            <SmallUserProfile
-              mode="Ladder"
-              avatarData={userB_avatar}
-              recordData={userB_record_}
-            />
+              <div className="flex flex-col items-center justify-center">
+              </div>
+            </div>
+              <div>
+                score
+              </div>
           </div>
-          <div
-            className={`w-full aspect-[4/2.2] bg-yellow-300 rounded-md`}
-          ></div>
         </div>
         <MiniChatting />
       </div>
     </div>
+    // ) : (
+      // <div>Game Over</div>
+    // )}
+  // );
+
+
+
+
+    // <div className="flex flex-col items-center h-screen max-h-screen w-screen max-w-screen pt-12">
+    //   <h1 className="text-3xl h-[5%] font-bold mb-10">GamePlayPage</h1>
+    //   <div className="w-full h-[85%] flex justify-center">
+    //     <div className="w-full lg:w-8/12 h-full mx-5">
+    //       <div className="flex bg-slate-200 h-[8rem] justify-evenly">
+    //         <SmallUserProfile
+    //           mode="Ladder"
+    //           avatarData={userA_avatar}
+    //           recordData={userA_record_}
+    //         />
+    //         <div className="flex w-1/6 items-center">
+    //           <h1 className="text-4xl">{`${userASetScore}`}</h1>
+    //           <div className="flex flex-col items-center justify-center">
+    //             <h1 className="text-2xl">Set</h1>
+    //             <h3 className="mx-12 whitespace-nowrap text-2xl">{`${userAScore}   :   ${userBScore}`}</h3>
+    //           </div>
+    //           <h1 className="text-4xl">{`${userBSetScore}`}</h1>
+    //         </div>
+    //         <SmallUserProfile
+    //           mode="Ladder"
+    //           avatarData={userB_avatar}
+    //           recordData={userB_record_}
+    //         />
+    //       </div>
+    //       <div
+    //         className={`w-full aspect-[4/2.2] bg-yellow-300 rounded-md`}
+    //       ></div>
+    //     </div>
+    //     <MiniChatting />
+    //   </div>
+    // </div>
   );
 }
 
