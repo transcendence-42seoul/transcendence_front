b88942e16f12b5086c89a4f4a8a7ef116fe72402
seokunee <xocoolree@gmail.com>
2023-11-13 02:46:00 +0900

    [refactor] user, game mock data 분리

--- src/recoil/gameAtom.ts
+++ src/recoil/gameAtom.ts
@@ -2,6 +2,7 @@ import { GameModeType } from './../game/ready/GameReadyPage';
 import { IGame, UserDataType } from '../game/ready/GameReadyPage';
 import { Ai, Ball, GameType } from './../game/play/pong/pong_engin';
 import { atom, selector } from 'recoil';
+import { GuestMock, HostMock } from '../__mock__/user/user.mock';
 
 // export const GameAtom = atom<GameType>({
 //   key: 'GameAtom',
@@ -18,94 +19,14 @@ import { atom, selector } from 'recoil';
 //   },
 // });
 
-const gameMock: IGame = {
-  end_time: '2021-08-31T14:00:00.000Z',
-  start_time: '2021-08-31T14:00:00.000Z',
-  game_mode: 1,
-  game_status: true,
-  gameHost_score: 1,
-  gameGuest_score: 3,
-  room_id: 'testRoomId',
-  idx: 300,
-};
-
-const hostMock: UserDataType = {
-  idx: 2000,
-  id: 'kunee',
-  nickname: 'anabada',
-  email: 'seokchoi@naver.com',
-  status: 'ONLINE',
-  tfa_enabled: false,
-  tfa_secret: null,
-  avatar: {
-    idx: 1,
-    image_data: {
-      type: 'string',
-      data: [1, 2, 3, 4],
-    },
-  },
-  record: {
-    idx: 2000,
-    total_game: 12,
-    total_win: 8,
-    ladder_game: 6,
-    ladder_win: 4,
-    general_game: 6,
-    general_win: 4,
-  },
-  host: gameMock,
-  guest: gameMock,
-  ranking: {
-    idx: 2000,
-    score: 2,
-  },
-  requester: [],
-  requested: [],
-  banner: [],
-};
-
-const guestMock: UserDataType = {
-  idx: 1000,
-  id: 'gtd9511',
-  nickname: 'iambluebird',
-  email: 'iam@bluebird.com',
-  status: 'ONLINE',
-  tfa_enabled: false,
-  tfa_secret: null,
-  avatar: {
-    idx: 1,
-    image_data: {
-      type: 'string',
-      data: [],
-    },
-  },
-  record: {
-    idx: 1000,
-    total_game: 1000,
-    total_win: 1000,
-    ladder_game: 500,
-    ladder_win: 500,
-    general_game: 500,
-    general_win: 500,
-  },
-  host: gameMock,
-  guest: gameMock,
-  ranking: {
-    idx: 1,
-    score: 3000,
-  },
-  requester: [],
-  requested: [],
-  banner: [],
-};
 export const GameUsersAtom = atom<{
   host: UserDataType;
   guest: UserDataType;
 }>({
   key: 'GameUsersInfoAtom',
   default: {
-    host: hostMock,
-    guest: guestMock,
+    host: HostMock,
+    guest: GuestMock,
   },
 });
 
