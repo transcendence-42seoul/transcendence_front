33e4decef75ae9033fb4590563f3447a88611d60
seokunee <xocoolree@gmail.com>
2023-12-04 22:21:26 +0900

    [fix] 온라인에서 챌린지 안되는 에러 수정 #78

--- src/client/MainPage.tsx
+++ src/client/MainPage.tsx
@@ -31,7 +31,7 @@ function MainPage() {
   const [chatRooms, setChatRooms] = useState<IChatRoom[]>([]);
   const [chatRoomAdded, setChatRoomAdded] = useState(true);
   const [activeTab, setActiveTab] = useState('lobby');
-
+  const [challengeUserIdx, setChallengeUserIdx] = useState<number>(0);
   const [selectedChat, setSelectedChat] = useState<IChatRoom | null>(null);
 
   const {
@@ -244,6 +244,7 @@ function MainPage() {
         user: online,
         position: { x: e.clientX, y: e.clientY },
       });
+      setChallengeUserIdx(online.idx);
     }
   };
 
@@ -281,6 +282,7 @@ function MainPage() {
         user: friend,
         position: { x: e.clientX, y: e.clientY },
       });
+      setChallengeUserIdx(friend.idx);
     }
   };
 
@@ -484,7 +486,7 @@ function MainPage() {
                   ))}
               </div>
               <CreateChallengeModal
-                requestedIdx={userIdx}
+                requestedIdx={challengeUserIdx}
                 modalState={{
                   isOpen: isCreateChallengeOpen,
                   onOpen: onOpenCreateChallenge,
